61480aca62b1f0e1e3b376985ce352bc1664b0a0
phosphor314 <phosphor314@outlook.com>
2026-05-18 16:41:53 +0200

    SFML hinzufügen

--- wordle_at_home/main.cpp
+++ wordle_at_home/main.cpp
@@ -1,6 +1,23 @@
-#include <iostream>
 #include "wordle.h"
+#include <SFML/Graphics.hpp>
+
 
 int main(){
-    std::cout << "Hello, World!\n";
+	sf::VideoMode videoMode = sf::VideoMode(sf::Vector2u{800, 800});
+	sf::RenderWindow window = sf::RenderWindow(videoMode, "wordle at home");
+	std::optional<sf::Event> windowEvent;
+	window.setFramerateLimit(60);
+
+	while (window.isOpen()) {
+		windowEvent = window.pollEvent();
+		while (windowEvent.has_value()) {
+			if (windowEvent.value().is<sf::Event::Closed>()) {
+				window.close();
+			}
+			windowEvent = window.pollEvent();
+		}
+
+		window.clear();
+		window.display();
+	}
 }
