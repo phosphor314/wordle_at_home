d9d3f70c281e1477d7fa99973f6d7c8585f74c0d
DasBrot25 <filip11krol@gmail.com>
2026-06-22 10:28:48 +0200

    words

--- wordle_at_home/wordle.cpp
+++ wordle_at_home/wordle.cpp
@@ -1,7 +1,35 @@
 #include "wordle.h"
+#include <fstream>
 
 std::vector<LetterStates> Wordle::getLetterStates(std::wstring userInput){
 	for (wchar_t c : userInput){
 	    
+	}
+}
+
+
+
+Wordle::Wordle()
+{
+	std::basic_ifstream<wchar_t> german;
+	german.open("german.txt", std::ios_base::ate);
+	std::vector<wchar_t> content;
+	content.resize(german.tellg());
+	german.seekg(0);
+	german.read(content.data(), content.size());
+
+	std::wstring zeichen;
+	for (wchar_t c : content) {
+		if (c == '\n') { 
+			words.push_back(zeichen);
+			zeichen = '';
+		}
+		else {
+			zeichen += c; 
+		}
+	}
+	if (zeichen != '') {
+		words.push_back(zeichen);
+		zeichen = '';
 	}
 }
