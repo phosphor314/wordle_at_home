659dd9cf77f96f8df725b6a0efac30ddbfbd8a80
phosphor314 <phosphor314@outlook.com>
2026-08-12 10:09:00 +0200

    add upgrades

--- wordle_at_home/constants.h
+++ wordle_at_home/constants.h
@@ -3,8 +3,13 @@
 #include <SFML/Graphics.hpp>
 #include <vector>
 
+enum Upgrades{
+    
+};
+
 struct Constants{
     Constants();
     sf::Font MONOSPACE_FONT = sf::Font("SpaceMono-Regular.ttf");
 	std::vector<std::wstring> words;
+	std::vector<Upgrades> upgrades;
 };
